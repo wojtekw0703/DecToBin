4f61a382819294784d415827768cbe2467f5d24c
Wojciech Wydmuch <33324211+wojtekw0703@users.noreply.github.com>
2019-11-05 18:19:52 +0100

    Add files via upload

---
+++ Zadanie 1/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void wczytywanie()
+{
+    int liczba;
+
+    printf("Podaj liczbe w systemie dziesietnym: ");
+    scanf("%d", &liczba);
+
+    if(liczba<0)
+    {
+        printf("Nie wpisujemy ujemnych liczb");
+    }else
+    {
+        binarnie(liczba);
+    }
+}
+
+bool sprawdzanie(int liczba)
+{
+
+}
+
+void binarnie(int liczba)
+{
+  long wynik=0,modulo,f=1;
+
+    while(liczba != 0)
+    {
+         modulo = liczba % 2;
+         wynik = wynik + modulo * f;
+         f = f * 10;
+         liczba = liczba / 2;
+    }
+   system ("cls");
+   printf("Postac binarna: %d", wynik);
+}
+
+int main()
+{
+   wczytywanie();
+
+   return 0;
+}
